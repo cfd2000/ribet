f3240d33c5463c92f6635de7ce9703e4d9eb0b3d
cfd2000 <52366440+cfd2000@users.noreply.github.com>
2021-01-27 21:48:24 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,7 @@
+<?php
+
+$__cfduid = "de9f78b8c0dba7015de2c1b90a12b25971610234155";
+$_ga = "GA1.2.145092982.1610234158";
+$cf_clearance = "4bd6eaceac29f4be6c9541c4b425557d20e4fab3-1611753317-0-150";
+$PHPSESSID = "shar7f8dfh69qqlo5kod69snr2";
+$_gid = "GA1.2.23192880.1611753323";
